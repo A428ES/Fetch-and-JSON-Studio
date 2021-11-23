fe03d964e255a737eb61768358b878b16d49b57d
Sergio Estrada <estrada.serg@gmail.com>
2021-11-22 19:34:57 -0600

    studio completion

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// TODO: add code here
+window.addEventListener("load", event => {
+    let jsonDat = [];
+    let content = document.getElementById("container");
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(event => {
+        event.json().then(astronautList => {
+
+            astronautList.sort(function(a, b){ return b.hoursInSpace - a.hoursInSpace });
+            let totalHTML = '';
+            for(let i = 0;i<astronautList.length;i++){
+            totalHTML += `<div class="astronaut">
+            <div class="bio">
+               <h3>${astronautList[i].firstName} ${astronautList[i].lastName}</h3>
+               <ul>
+                  <li>${astronautList[i].hoursInSpace}</li>
+                  <li`;
+                  
+                  if(astronautList[i].active === true) {
+                      totalHTML += ` class="active"`;
+                  } else {
+                      totalHTML += ``;
+                  }
+                  
+                  totalHTML += `>Active: ${astronautList[i].active}</li>
+                  <li>Skills: ${astronautList[i].skills}</li>
+               </ul>
+            </div>
+            <img class="avatar" src="${astronautList[i].picture}">
+         </div>`;
+
+         content.innerHTML = totalHTML;
+            }
+            
+        });
+    })
+});
